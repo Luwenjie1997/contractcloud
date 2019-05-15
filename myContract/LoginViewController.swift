46fa8eaf921f1f61938492017da677ea9d3ef16f
卢文杰 <27827478@qq.com>
2019-05-15 23:36:41 +0800

    register and login and mainInterface done

--- myContract/LoginViewController.swift
+++ myContract/LoginViewController.swift
@@ -17,25 +17,60 @@ class LoginViewController: UIViewController {
         
         
     }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let identifier = segue.identifier{
+//            switch identifier{
+//            case "UserLoginSuccessly" :
+//                if let vc = segue.destination as? MainIntViewController{
+//                    print("hehehe")
+//                }
+//            default:
+//                break
+//            }
+//        }
+//    }
 
     @IBOutlet weak var LoginPhoneNumber: UITextField!
     @IBOutlet weak var LoginPassword: UITextField!
     @IBAction func clickLoginButton(_ sender: Any) {
-        print("aaa")
         let phoneNumber = LoginPhoneNumber.text ?? ""
         let password = LoginPassword.text ?? ""
         LCUser.logIn(mobilePhoneNumber: phoneNumber, password: password){result in
             switch result{
             case .success(let user):
+//                let vc = self.storyboard?.instantiateViewController(withIdentifier: "MainInterface")
+//                self.navigationController?.pushViewController(vc!, animated: true)
+//                break
+                let sb = UIStoryboard(name: "Main", bundle: nil)
+//                let vc = sb.instantiateViewController(withIdentifier: "tabBarController")
+//                print(vc)
+//                self.present(vc, animated: true, completion: nil)
+                var vc :MainInterfaceViewController!
+                vc = sb.instantiateViewController(withIdentifier: "tabBarController") as! MainInterfaceViewController
+                vc.MainUser = user
+                print(vc.MainUser?.mobilePhoneNumber)
+                self.present(vc, animated: true, completion: nil)
                 break
             case .failure(let error):
                 print(error)
+                
                 let alert = UIAlertController(title: "账号密码错误", message: "请重新输入", preferredStyle: .alert)
                 let action = UIAlertAction(title: "确定", style: .default, handler: nil)
                 alert.addAction(action)
                 self.present(alert, animated: true, completion: nil)
-                
+                break
+
             }
+//            if (result.error != nil) {
+////                print(result.error)
+//                let alert = UIAlertController(title: result.error?.failureReason, message: "请重新输入", preferredStyle: .alert)
+//                let action = UIAlertAction(title: "确定", style: .default, handler: nil)
+//                alert.addAction(action)
+//                self.present(alert, animated: true, completion: nil)
+//            }else{
+//
+//            }
         }
         
     }
