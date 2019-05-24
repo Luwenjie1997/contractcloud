3fa609e4c1d65bcc16bcdedf9f6d741094b5e255
卢文杰 <27827478@qq.com>
2019-05-25 01:36:12 +0800

    signature

--- contractPDF/addNewContractViewController.swift
+++ contractPDF/addNewContractViewController.swift
@@ -10,8 +10,38 @@ import UIKit
 import LeanCloud
 import PDFKit
 
+
+class ImageStampAnnotation: PDFAnnotation {
+    
+    var image: UIImage!
+    
+    // A custom init that sets the type to Stamp on default and assigns our Image variable
+    init(with image: UIImage!, forBounds bounds: CGRect, withProperties properties: [AnyHashable : Any]?) {
+        super.init(bounds: bounds, forType: PDFAnnotationSubtype.stamp, withProperties: properties)
+        
+        self.image = image
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func draw(with box: PDFDisplayBox, in context: CGContext) {
+        
+        // Get the CGImage of our image
+        guard let cgImage = self.image.cgImage else { return }
+        
+        // Draw our CGImage in the context of our PDFAnnotation bounds
+        context.draw(cgImage, in: self.bounds)
+        
+    }
+}
+
 class addNewContractViewController: UIViewController {
 
+    var signatureImage :UIImage?
+    var currentlySelectedAnnotation: PDFAnnotation?
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var titleOfAdd: UILabel!
@@ -29,6 +59,8 @@ class addNewContractViewController: UIViewController {
                 pdfView.autoScales = true
                 button1.isHidden = true
                 button2.isHidden = true
+                let panAnnotationGesture = UIPanGestureRecognizer(target: self, action: #selector(didPanAnnotation(sender:)))
+                pdfView.addGestureRecognizer(panAnnotationGesture)
                 
             }
         }
@@ -48,7 +80,10 @@ class addNewContractViewController: UIViewController {
             pdfView.sizeToFit()
             pdfView.autoScales = true
             button1.isHidden = false
-            button2.isHidden = true
+            button2.isHidden = false
+            pdfView.backgroundColor = UIColor.lightGray
+            let panAnnotationGesture = UIPanGestureRecognizer(target: self, action: #selector(didPanAnnotation(sender:)))
+            pdfView.addGestureRecognizer(panAnnotationGesture)
             let file = LCFile(payload: .fileURL(fileURL: url))
             let userPhone = DataHandle.shareInstence.currentUser!.mobilePhoneNumber!.jsonString.trimmingCharacters(in: .punctuationCharacters)
             try?file.set("user", value: userPhone)
@@ -73,5 +108,51 @@ class addNewContractViewController: UIViewController {
 
     @IBAction func send(_ sender: Any) {
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        guard let signatureImage = signatureImage,
+            let page = pdfView.currentPage else { return }
+        let pageBounds = page.bounds(for: .cropBox)
+        // lets just add the image to the center of the pdf page with a width of 200px and a height of 100 px
+        let imageBounds = CGRect(x: pageBounds.midX, y: pageBounds.midY,  width: 200, height: 100)
+        // Now we can add our stamp as a annotation of the current pdf page
+        let imageStamp = ImageStampAnnotation(with: signatureImage,  forBounds: imageBounds, withProperties: nil)
+        page.addAnnotation(imageStamp)
+    }
+    
+    
+    @objc func didPanAnnotation(sender: UIPanGestureRecognizer) {
+        let touchLocation = sender.location(in: pdfView)
+        guard let page = pdfView.page(for: touchLocation, nearest: true)
+            else {
+                return
+        }
+        let locationOnPage = pdfView.convert(touchLocation, to:  page)
+        switch sender.state {
+        case .began:
+            guard let annotation = page.annotation(at: locationOnPage) else {   return }
+            if annotation.isKind(of: ImageStampAnnotation.self) {
+                currentlySelectedAnnotation = annotation
+            }
+        case .changed:
+            guard let annotation = currentlySelectedAnnotation else {return }
+            let initialBounds = annotation.bounds
+            // Set the center of the annotation to the spot of our finger
+            annotation.bounds = CGRect(x: locationOnPage.x - (initialBounds.width / 2), y: locationOnPage.y - (initialBounds.height / 2), width: initialBounds.width, height: initialBounds.height)
+        case .ended, .cancelled, .failed:
+            currentlySelectedAnnotation = nil
+        default:
+            break
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showSignatureSegue" {
+            if let nextVC = segue.destination as? SignatureViewController {
+                nextVC.previousViewController = self
+            }
+        }
     }
 }
---
+++ signature/SignatureViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SignatureViewController.swift
+//  myContract
+//
+//  Created by 卢文杰 on 2019/5/25.
+//  Copyright © 2019 lu. All rights reserved.
+//
+
+import UIKit
+
+class SignatureViewController: UIViewController,YPSignatureDelegate {
+    
+    @IBOutlet weak var signature: YPDrawSignatureView!
+    var image : UIImage?
+    var previousViewController: UIViewController?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        signature.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        image = self.signature.getSignature()
+        print(image)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let VC = storyboard.instantiateViewController(withIdentifier: "newContractStoryboard") as? addNewContractViewController
+        VC?.signatureImage = image!
+        self.navigationController?.pushViewController(VC!, animated: true)
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        self.signature.clear()
+    }
+    
+    
+    func didStart(_ view : YPDrawSignatureView) {
+        print("Started Drawing")
+    }
+    
+    // didFinish() is called rigth after the last touch of a gesture is registered in the view.
+    // Can be used to enabe scrolling in a scroll view if it has previous been disabled.
+    func didFinish(_ view : YPDrawSignatureView) {
+        print("Finished Drawing")
+    }    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
+//extension SignatureViewController: UINavigationControllerDelegate {
+//
+//    func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+//        if viewController == self.previousViewController {
+//            let vc = viewController as! signViewController
+//            vc.signatureImage = image
+//            print(image)
+//        }
+//    }
+//
+//}
+
